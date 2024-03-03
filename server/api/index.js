0020ee660c2e2d776bc48fb063a047001247e5ce
Dennis Yeung <10432376+otyeung@users.noreply.github.com>
2024-03-04 00:13:56 +0800

    modify

--- server/api/index.js
+++ server/api/index.js
@@ -24,7 +24,27 @@ const googleFormFields = {
   oracleMoatId: '48487709',
 }
 
-app.use(cors())
+app.use((req, res, next) => {
+  const origin = req.get('referer')
+  const isWhitelisted = whitelist.find((w) => origin && origin.includes(w))
+  if (isWhitelisted) {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader(
+      'Access-Control-Allow-Methods',
+      'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+    )
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-Requested-With,Content-Type,Authorization'
+    )
+    res.setHeader('Access-Control-Allow-Credentials', true)
+  }
+  // Pass to next layer of middleware
+  if (req.method === 'OPTIONS') res.sendStatus(200)
+  else next()
+})
+
+//app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
