0ebc5c8d843fb175ec4c3e01fef9db5fd9df11ea
Dennis Yeung <10432376+otyeung@users.noreply.github.com>
2024-03-07 13:35:31 +0800

    add force IPv4

--- server/api/index.js
+++ server/api/index.js
@@ -1,9 +1,13 @@
+// Import necessary modules
 const express = require('express')
 const bodyParser = require('body-parser')
 const axios = require('axios')
 const cors = require('cors')
+
+// Create an Express app
 const app = express()
 
+// Define mapping between form fields and Google Form entry ids
 const googleFormFields = {
   li_fat_id: '844537053',
   lastName: '234622488',
@@ -18,14 +22,16 @@ const googleFormFields = {
   oracleMoatId: '48487709',
 }
 
-// Add supported origin domain to whitelist, or use *
+// Define a whitelist of allowed origins for CORS
 const whitelist = [
   'https://linkedin-online-capi-dreamdata.vercel.app',
   'http://localhost:3000',
 ]
 
+// Configure CORS options
 const corsOptions = {
   origin: function (origin, callback) {
+    // Check if the origin is whitelisted
     const isWhitelisted = whitelist.includes('*') || whitelist.includes(origin)
     callback(null, isWhitelisted)
   },
@@ -41,18 +47,22 @@ const corsOptions = {
   exposedHeaders: ['Content-Type'], // Add headers that you want to expose to the frontend.
 }
 
+// Use CORS middleware with the specified options
 app.use(cors(corsOptions))
+
+// Use body-parser middleware to parse JSON and url-encoded data
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-// GET route for the root endpoint
+// Define a GET route for the root endpoint
 app.get('/', (req, res) => {
   res.send(`Server is running on port 4000.`) // Assuming your server is running on port 4000
 })
 
-// POST route for form submission
+// Define a POST route for handling form submissions to Google Forms
 app.post('/submit-google-form', async (req, res) => {
   try {
+    // Extract form data from the request
     const formData = req.body
     const formDataEntries = Object.entries(formData)
 
@@ -64,11 +74,14 @@ app.post('/submit-google-form', async (req, res) => {
       })
       .join('&')
 
-    // Send a POST request to Google Forms
+    // Send a POST request to Google Forms with forced IPv4
     await axios.post(
       'https://docs.google.com/forms/d/e/1FAIpQLSe-8d-XpykGeLDdGzroOw8hXgE0L_HqUnEw1VH6qBIlnFSeKg/formResponse?usp=pp_url',
       postData,
-      { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }
+      {
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        family: 4, // Force IPv4
+      }
     )
 
     console.log('Form submitted successfully')
@@ -81,6 +94,8 @@ app.post('/submit-google-form', async (req, res) => {
   }
 })
 
+// Start the server on port 4000
 app.listen(4000, () => console.log('Server ready on port 4000.'))
 
+// Export the Express app
 module.exports = app
